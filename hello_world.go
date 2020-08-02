1e2ea093ef46d7efe70b631206653cf446a44513
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-08-02 12:21:41 -0700

    added hello world

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+package main
+
+// double quotes are important
+// fmt is formatting library provided by golang, lets us print to terminal and shit
+import "fmt"
+
+func main() {
+	// access the Println function from the fmt library
+	// functions from a library start with a capital letter
+	fmt.Println("Hello World this is my first go program :)")
+}
