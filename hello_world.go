8a785c61c1cfd6863bfead7416525d519310956c
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-08-03 14:24:50 -0700

    values

--- hello_world.go
+++ hello_world.go
@@ -13,4 +13,12 @@ func main() {
 	// functions from a library start with a capital letter
 	fmt.Println("Hello World this is my first go program :)")
 	fmt.Println(uuid.New().String())
+
+	// values
+	fmt.Println("string " + "concatination")
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
